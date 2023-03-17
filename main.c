87aa6fd4b46d46651338552fc15d8d17dc15317b
Przemysław Solniczek <przemeksol@wp.pl>
2023-03-17 11:22:02 +0100

    demon

--- main.c
+++ main.c
@@ -129,6 +129,40 @@ void syncDirDemon()
 
 }
 
+int demon()
+{
+    pid_t pid;   
+    int i;
+
+    /* stwórz nowy proces */
+    pid = fork ( );   
+    if (pid == -1)      
+        return -1;  
+    else if (pid != 0)
+        exit (EXIT_SUCCESS);
+
+    /* stwórz nową sesję i grupę procesów */
+    if (setsid ( ) == -1)
+        return -1;
+
+    /* ustaw katalog roboczy na katalog główny */
+    if (chdir ("/") == -1)
+        return -1;
+
+    /* zamknij wszystkie pliki otwarte - użycie opcji NR_OPEN to przesada, lecz działa */
+    for (i = 0; i < 1024; i++)
+        close (i);
+
+    /* przeadresuj deskryptory plików 0, 1, 2 na /dev/null */
+    open ("/dev/null", O_RDWR);/* stdin */
+    dup (0);/* stdout */
+    dup (0); /* stderror */
+
+    /* tu należy wykonać czynności demona... */ 
+    
+    return 0;
+}
+
 int checkParameters(int argc, char *argv[], char **source_dir, char **dest_dir, unsigned int *interval, int *recursive, unsigned long long *threshold)
 {
     // Jeżeli podano za mało argumentów
